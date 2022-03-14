f38198847aec71f71b9943853f92b02f7be5c95c
vadim-rm <37626101+vadim-rm@users.noreply.github.com>
2022-03-14 22:39:32 +0600

    Update DockerFile

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/proton-bit/NTO_ATC.git /home/dev_ws/src/car -b main
+git clone https://github.com/proton-bit/SelfDrivingCar.git /home/dev_ws/src/car -b main
 python3 -m pip install -r /home/dev_ws/src/car/car/requirements.txt
 colcon build --symlink-install
